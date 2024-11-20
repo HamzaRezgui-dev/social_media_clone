784c5f6b3256b805b46861abd5963258c7399f4d
ChazzitUP <hamzarezgui.education@gmail.com>
2024-11-20 19:55:36 +0100

    first commit

---
+++ api/routes/auth.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { login, register, logout } from "../controllers/auth.js";
+const router = express.Router();
+
+router.post("/login", login);
+router.post("/login", register);
+router.post("/login", logout);
+
+export default router;
